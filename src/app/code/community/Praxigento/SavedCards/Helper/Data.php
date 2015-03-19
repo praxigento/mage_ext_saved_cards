726ddfcea908e1e0a8bebe07ea9dbbb77ac07fcf
Alex Gusev <alex@flancer64.com>
2015-03-19 16:32:36 +0200

    AD-109 - Create stub for Praxigento_SavedCards module

---
+++ src/app/code/community/Praxigento/SavedCards/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) 2015, Praxigento
+ * All rights reserved.
+ */
+
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+class Praxigento_SavedCards_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
