de79babd178c92813e7f300b4ca2fbb2a4400f7b
Alex Gusev <alex@flancer64.com>
2015-03-20 16:34:55 +0200

    AD-129 - Move payment method to SavedCards module

--- src/app/code/community/Praxigento/SavedCards/Block/Own/Payment/Method/Form.php
+++ src/app/code/community/Praxigento/SavedCards/Block/Own/Payment/Method/Form.php
@@ -9,11 +9,14 @@
  */
 class Praxigento_SavedCards_Block_Own_Payment_Method_Form extends Mage_Payment_Block_Form_Cc
 {
-    const DOM_ID_SELECTOR   = 'prxgt_autoship_prxgt_saved_card_id';
-    const DOM_ID_WRAPPER    = 'prxgt_autoship_cc_wrapper';
+    /**
+     * ID for DOM element: payment method selector (radio button).
+     */
+    const DOM_ID_SELECTOR = 'prxgt_savedcards_method_prxgt_saved_card_id';
+    const DOM_ID_WRAPPER = 'prxgt_savedcards_cc_wrapper';
     const FLD_SAVED_CARD_ID = 'prxgt_saved_card_id';
-    const FLD_SAVE_FLAG     = 'prxgt_save_card';
-    const VAL_NEW_CARD      = 'prxgt_autoship_new_ccard';
+    const FLD_SAVE_FLAG = 'prxgt_saved_card_flag';
+    const VAL_NEW_CARD = 'prxgt_saved_card_flag';
 
     /** @var Praxigento_SavedCards_Mysql4_Own_Registry_Card_Collection */
     private $_savedCards = null;
@@ -23,6 +26,35 @@ class Praxigento_SavedCards_Block_Own_Payment_Method_Form extends Mage_Payment_B
         parent::_construct();
         $this->setTemplate('prxgt/savedcards/payment/form.phtml');
 //        $this->_savedCards = $this->loadCustomerSavedCards();
+        $this->_savedCards = array('q', 'w', 'e', 'r');
     }
 
+    public function canDisplaySaveCardCheckbox()
+    {
+        $result = true;
+        return $result;
+    }
+
+    public function hasSavedCreditCards()
+    {
+        $result = true;
+        return $result;
+    }
+
+    public function getSavedCards()
+    {
+        return $this->_savedCards;
+    }
+
+    public function uiCardId($card)
+    {
+        $result = "ID";
+        echo $result;
+    }
+
+    public function uiCardValue($card)
+    {
+        $result = "card number";
+        echo $result;
+    }
 }
